77f736b4df9f5c4c1c88fbdb7217266f75eaca6e
Yasser Carreón <yacasia@gmail.com>
2019-08-20 11:44:03 -0500

    [LS3] Create playground

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
 
+const Comment = ({ text }) => {
+  const emojied = text.replace(':)', ':smile:');
+
+  return (
+    <span>
+      {emojied}
+    </span>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      
+      <Comment text="This is a smiling face > :)" />
     </div>
   );
 }

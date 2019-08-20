ee648118446d11200867b9650135017f6754bc11
Yasser Carreón <yacasia@gmail.com>
2019-08-20 10:10:24 -0500

    [LS2] No wrapping multiple elements rendering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
 
+/*
+  Render multiple elements without a wrapping element.
+*/
+const Stuff = () => [
+  <li key="1">Truck</li>,
+  <li key="2">Tomatoe</li>,
+  <li key="3">Sugar</li>,
+];
+
 function App() {
   return (
     <div className="App">
-      
+      <ul>
+        <li>Car</li>
+        <li>Hammer</li>
+        <li>Flower</li>
+        <Stuff />
+      </ul>
     </div>
   );
 }

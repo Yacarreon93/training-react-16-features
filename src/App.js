64996158ed91d3a06b95499a5fe8c615b678738c
Yasser Carreón <yacasia@gmail.com>
2019-08-20 09:09:12 -0500

    [LS1] Implement "componentDidCatch" example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+/*
+  IN CASE OF STILL SEEING THE ERROR IN BROWSER:
+
+  "The 'create-react-app' package has a tool called the 'react-overlay-error'.
+  This shows error messages from the console as an overlay over your app so you can easily check the stack trace and debug.
+  This won't show up in production mode, it's just a development tool duplicating the normal browser console.
+  You can hide this by pressing Escape to see your overlay again."
+
+  Ref: https://stackoverflow.com/questions/52096804/react-still-showing-errors-after-catching-with-errorboundary
+*/
+
 const Profile = props => (
   <div>{props.user.name}</div>
 );
@@ -8,7 +19,16 @@ const Profile = props => (
 class App extends Component {
   state = {
     user: { name: 'Arturo' },
+    hasError: false,
   };
+
+  /*
+    Doc: https://es.reactjs.org/docs/react-component.html#componentdidcatch
+  */
+  componentDidCatch(error, info) {
+    console.log('componentDidCatch >', error, info);
+    this.setState({ hasError: true });
+  }
 
   /*
     Click on button to cause an error,
@@ -16,14 +36,22 @@ class App extends Component {
   updateUser = () => this.setState({ user: null });
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Profile user={this.state.user} />
-          <button onClick={this.updateUser}>Update</button>
-        </header>
-      </div>
-    );
+    if (this.state.hasError) {
+      return (
+        <div style={{ color: 'red' }}>
+          Something went wrong!
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <Profile user={this.state.user} />
+            <button onClick={this.updateUser}>Update</button>
+          </header>
+        </div>
+      );
+    }
   }
 }
 

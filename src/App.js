cd8d41a169a34ef56848913440b4e3ec1017f91c
Yasser Carreón <yacasia@gmail.com>
2019-08-20 22:34:42 -0500

    [LS4] Simple Portal example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>My app</h1>
+      {/*
+        Portals provide a first-class way to render children into a DOM
+        node that exists outside the DOM hierarchy of the parent component.
+
+        Doc: https://reactjs.org/docs/portals.html
+
+        Don't forget to include the container into "index.html".
+      */}
+      {ReactDOM.createPortal(
+        <div>Welcome</div>,
+        document.getElementById('portal-container'),
+      )}
     </div>
   );
 }

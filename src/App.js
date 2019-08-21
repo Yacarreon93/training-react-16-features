5984fc0d9ba3fca0e3ca5932356d596bfabd68e9
Yasser Carreón <yacasia@gmail.com>
2019-08-20 22:56:19 -0500

    [LS4] Create an Overlay component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My app</h1>
-      {/*
-        Portals provide a first-class way to render children into a DOM
-        node that exists outside the DOM hierarchy of the parent component.
+class Overlay extends Component {
+  constructor(props) {
+    super(props);
 
-        Doc: https://reactjs.org/docs/portals.html
+    /*
+      It creates its own container in the DOM.
+    */
+    this.overlayContainer = document.createElement('div');
 
-        Don't forget to include the container into "index.html".
-      */}
-      {ReactDOM.createPortal(
-        <div>Welcome</div>,
-        document.getElementById('portal-container'),
-      )}
-    </div>
-  );
+    document.body.appendChild(this.overlayContainer);
+  }
+
+  /*
+    The container is removed when the component is unmounted.
+  */
+  componentWillUnmount() {
+    document.body.removeChild(this.overlayContainer);
+  }
+
+  render() {
+    return ReactDOM.createPortal(
+      <div className="overlay">
+        {this.props.children}        
+        <span onClick={this.props.onClose}>X</span> {/* Click here to close overlay */}
+      </div>,
+      this.overlayContainer,
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { overlayActive: true };
+  }
+
+  closeOverlay = () => {
+    this.setState({ overlayActive: false });
+  };
+
+  render() {
+
+    return (
+      <div className="App">
+        <h1>My app</h1>
+        {/*
+          Portals provide a first-class way to render children into a DOM
+          node that exists outside the DOM hierarchy of the parent component.
+  
+          Doc: https://reactjs.org/docs/portals.html
+  
+          Don't forget to include the container into "index.html".
+        */}
+        {this.state.overlayActive && (
+          <Overlay onClose={this.closeOverlay}>
+            <span>Welcome</span>
+          </Overlay>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;

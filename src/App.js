b83115b7608cf48aaa0120911ecf6061215c4202
Yasser Carreón <yacasia@gmail.com>
2019-09-05 10:34:37 -0500

    [LS5] Cases when React will pass attrs in string

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ import './App.css';
   an attribute that React doesn’t recognize, React would just skip it.
 */
 
+class Foo {
+  toString() {
+    return 'foo';
+  }
+}
+
+const foo = new Foo();
+
 function App() {
   return (
     <div
@@ -33,10 +41,18 @@ function App() {
       onion="true"
       ontas="true"
       onclick={() => console.log('clicked')}
-      // Cases when React will not render the attributes:
+      // Cases when React will NOT render the attributes:
       wrong-attribute-1={true}
       wrong-attribute-2={() => null}
       wrong-attribute-3={Symbol('foo')}
+      /*
+        Cases when React will render the attributes
+        converting them to string using the "toString" method.
+      */
+     custom-attr-1={5}
+     custom-attr-2={NaN}
+     custom-attr-3={{ foo: 'bar' }}
+     custom-attr-4={foo}
     >
       Hello wordl!
     </div>

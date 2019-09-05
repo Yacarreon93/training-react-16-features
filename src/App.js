b15b9f021b1ed95fe7e868ff06859c1a1afab3b8
Yasser Carreón <yacasia@gmail.com>
2019-09-05 11:04:41 -0500

    [LS6] Create playground

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  state = {
+    fruit: 'apple',
+    loading: false,
+  };
+
+  handleFruitChange = evt => {
+    evt.persist();
+
+    const fruit = evt.currentTarget.value;
+
+    this.setState({ loading: true }, () => {
+      setTimeout(() => this.setState({
+        fruit,
+        loading: false,
+      }), 1000);
+    })
+  }
+
+  renderFruit = () => this.state.loading ? 'loading' : this.state.fruit;
+
+  render() {
+    return (
+      <div className="App">
+        <button
+          type="button"
+          value="apple"
+          onClick={this.handleFruitChange}
+        >
+          Apple
+        </button>
+        <button
+          type="button"
+          value="banana"
+          onClick={this.handleFruitChange}
+        >
+          Banana
+        </button>
+        {this.renderFruit()}
+      </div>
+    );
+  }
 }
 
 export default App;

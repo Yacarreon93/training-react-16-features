58227881071816497af4f0c23083ba2432924e96
Yasser Carreón <yacasia@gmail.com>
2019-09-05 01:17:27 -0500

    [LS5] Add custom attributes to DOM element

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
 
+/*
+  Doc: https://es.reactjs.org/blog/2017/09/08/dom-attributes-in-react-16.html
+
+  In the past React used to ignore unknown DOM attributes. If you wrote JSX with
+  an attribute that React doesn’t recognize, React would just skip it.
+*/
+
 function App() {
   return (
-    <div className="App">
-      
+    <div
+      // Open the browser inspector to see the attrs.
+      my-attribute-1="foo"
+      my-attribute-2="var"
+    >
+      Hello wordl!
     </div>
   );
 }

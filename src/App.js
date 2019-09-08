040f31559b6e93c906d9882543770f3960d180ff
Yasser Carreón <yacasia@gmail.com>
2019-09-07 20:33:33 -0500

    [LS6] Use of setState updater

--- src/App.js
+++ src/App.js
@@ -3,52 +3,39 @@ import './App.css';
 
 // See doc: https://reactjs.org/docs/react-component.html#setstate
 
-class App extends Component {
-  state = {
-    fruit: 'apple',
-    loading: false,
-  };
-
-  handleFruitChange = evt => {
-    evt.persist();
-
-    const fruit = evt.currentTarget.value;
-
-    const callback = () => setTimeout(() => this.setState({
-      fruit,
-      loading: false,
-    }), 1000);
-
-    /*
-      setState() does not always immediately update the component.
-      It may batch or defer the update until later. This makes reading
-      this.state right after calling setState() a potential pitfall.
-      Instead, use setState callback (setState(updater, callback)),
-      it is guaranteed to fire after the update has been applied.
-    */
-    this.setState({ loading: true }, callback);
+/*
+  Both state and props received by the updater function are guaranteed to be up-to-date.
+  The output of the updater is shallowly merged with state.
+  It's possible to not update the state once setState() is called, returning "null" will make
+  the state no change, you can see there is no more "it renders" when the counter reachs 5.
+*/
+const updater = (state, props) => {
+  if (state.counter >= 5) {
+    return null;
   }
 
-  renderFruit = () => this.state.loading ? 'loading...' : this.state.fruit;
+  return { counter: state.counter + 1 };
+}
+
+class App extends Component {
+  state = {
+    counter: 0,
+  };
+
+  sumOne = () => this.setState(updater);
 
   render() {
+    console.log('it renders');
+
     return (
       <div className="App">
         <button
           type="button"
-          value="apple"
-          onClick={this.handleFruitChange}
+          onClick={this.sumOne}
         >
-          Apple
+          +
         </button>
-        <button
-          type="button"
-          value="banana"
-          onClick={this.handleFruitChange}
-        >
-          Banana
-        </button>
-        {this.renderFruit()}
+        {this.state.counter}
       </div>
     );
   }

a357e921343f35095d2d90553642a389e9f6be96
Yasser Carreón <yacasia@gmail.com>
2019-09-08 17:23:28 -0500

    [LS8] The use of createRef

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+// See doc: https://reactjs.org/docs/refs-and-the-dom.html
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    /*
+      Refs provide a way to access DOM nodes or React elements created in the render method.
+    */
+    this.inputRef = React.createRef();
+    console.log('constructor', this.inputRef.current); // Ref is not accessible at this point.
+  }
+  
+  componentDidMount() {
+    console.log('componentDidMount', this.inputRef.current); // Once the component did mount the ref is now accessible.
+    this.inputRef.current.focus();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* Ref attached to React elements via the ref attribute */}
+        <input type="text" ref={this.inputRef} />
+      </div>
+    );
+  }
 }
 
 export default App;

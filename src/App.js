bad1e5987c201923fbba2ae5377f6f77c91eeaca
Yasser Carreón <yacasia@gmail.com>
2019-09-08 15:44:36 -0500

    [LS7] Create playground

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
 
+const Fruits = () => [
+  <li key="1">Apple</li>,
+  <li key="2">Orange</li>,
+  <li key="3">Watermelon</li>,
+];
+
 function App() {
   return (
     <div className="App">
-      
+      <ul>
+        <li>Peach</li>
+        <li>Banana</li>
+        <Fruits />
+      </ul>
     </div>
   );
 }

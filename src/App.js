ac14c59c793dd2c902f31963e3b79b013559cdac
Yasser Carreón <yacasia@gmail.com>
2019-09-09 10:35:19 -0500

    [LS9] Implement HOC component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,27 @@ import './App.css';
 // https://reactjs.org/docs/forwarding-refs.html#forwarding-refs-to-dom-components
 // https://reactjs.org/docs/react-api.html#reactforwardref
 
+// NOTE: THIS CODE WILL THROW ERRORS WHEN CLICKING THE BUTTON!
+
+function logProps(WrappedComponent) {
+  class LogProps extends React.Component {
+    componentDidUpdate(prevProps) {
+      console.log('old props:', prevProps);
+      console.log('new props:', this.props);
+    }
+
+    render() {
+      /*
+        Refs will not get passed through. That’s because ref is not a prop.
+        Like key, it’s handled differently by React.
+      */
+      return <WrappedComponent {...this.props} />;
+    }
+  }
+
+  return LogProps;
+}
+
 class TextInput extends Component {
   inputRef = React.createRef();
 
@@ -13,6 +34,8 @@ class TextInput extends Component {
     return <input ref={this.inputRef} />;
   }
 };
+
+const HOCedTextInput = logProps(TextInput);
 
 /*
   ForwardedTextInput uses React.forwardRef() to obtain the ref passed to it,
@@ -28,7 +51,13 @@ class App extends Component {
   render () {
     return (
       <div className="App">
-        <TextInput ref={this.inputRef} />
+        {/* 
+          The TextInput component is wrapped by the LogProps HOC.
+          Even though the rendered output will be the same, our ref
+          will point to LogProps instead of the inner TextInput component!
+          This means we can't call e.g. ref.current.focus().
+        */}
+        <HOCedTextInput ref={this.inputRef} />
         <button onClick={this.focusInput}>
           Focus
         </button>

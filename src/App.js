449657bd21b4c4d07e6086744238c5f804c5a550
Yasser Carreón <yacasia@gmail.com>
2019-09-09 10:25:44 -0500

    [LS9] Focus input when clicking button

--- src/App.js
+++ src/App.js
@@ -4,25 +4,34 @@ import './App.css';
 // https://reactjs.org/docs/forwarding-refs.html#forwarding-refs-to-dom-components
 // https://reactjs.org/docs/react-api.html#reactforwardref
 
-const TextInput = (props, ref) => <input ref={ref} />;
+class TextInput extends Component {
+  inputRef = React.createRef();
+
+  focus = () => this.inputRef.current.focus();
+
+  render() {
+    return <input ref={this.inputRef} />;
+  }
+};
 
 /*
   ForwardedTextInput uses React.forwardRef() to obtain the ref passed to it,
   and then forward it to the DOM button that it renders.
 */
-const ForwardedTextInput = React.forwardRef(TextInput);
+// const ForwardedTextInput = React.forwardRef(TextInput);
 
 class App extends Component {
   inputRef = React.createRef();
 
-  componentDidMount() {
-    this.inputRef.current.focus();
-  }
+  focusInput = () => this.inputRef.current.focus();
 
   render () {
     return (
       <div className="App">
-        <ForwardedTextInput ref={this.inputRef} />
+        <TextInput ref={this.inputRef} />
+        <button onClick={this.focusInput}>
+          Focus
+        </button>
       </div>
     );
   }

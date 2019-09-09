460e2abec89face9390cf05bccf97f11d1fad168
Yasser Carreón <yacasia@gmail.com>
2019-09-09 09:57:20 -0500

    [LS9] Use of forwardRef

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+// https://reactjs.org/docs/forwarding-refs.html#forwarding-refs-to-dom-components
+// https://reactjs.org/docs/react-api.html#reactforwardref
+
+const TextInput = (props, ref) => <input ref={ref} />;
+
+/*
+  ForwardedTextInput uses React.forwardRef() to obtain the ref passed to it,
+  and then forward it to the DOM button that it renders.
+*/
+const ForwardedTextInput = React.forwardRef(TextInput);
+
+class App extends Component {
+  inputRef = React.createRef();
+
+  componentDidMount() {
+    this.inputRef.current.focus();
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <ForwardedTextInput ref={this.inputRef} />
+      </div>
+    );
+  }
 }
 
 export default App;

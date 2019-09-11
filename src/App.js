9fffe84ddba10ec35ff97c44dd04f08c46f751a9
Yasser Carreón <yacasia@gmail.com>
2019-09-11 11:28:02 -0500

    [LS10] Create playground

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class FetchJSON extends Component {
+  state = {
+    data: null,
+    isLoading: true,
+  };
+
+  fetchAndUpdate = async () => {
+    const response = await fetch(this.props.url);
+    const result = await response.json();
+
+    this.setState({
+      data: result,
+      isLoading: false,
+    });
+  };
+
+  componentDidMount() {
+    this.fetchAndUpdate();
+  }
+
+  render() {
+    return <Fragment>{this.props.render(this.state)}</Fragment>
+  }
+}
+
+class App extends Component {
+  state = {
+    person: 1,
+  };
+  
+  render() {
+    return (
+      <div className="App">
+        <FetchJSON
+          url={`https://swapi.co/api/people/${this.state.person}`}
+          render={({ isLoading, data }) => (
+            <div>{isLoading ? 'Loading...': `Name: ${data.name}`}</div>
+          )}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;

984501bfd4799844092db07ead2ff43829b6d392
Yasser Carreón <yacasia@gmail.com>
2019-09-13 10:37:20 -0500

    [LS6] Fix typo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
   Both state and props received by the updater function are guaranteed to be up-to-date.
   The output of the updater is shallowly merged with state.
   It's possible to not update the state once setState() is called, returning "null" will make
-  the state no change, you can see there is no more "it renders" when the counter reachs 5.
+  the state no change, you can see there is no more "it renders" when the counter reaches 5.
 */
 const updater = (state, props) => {
   if (state.counter >= 5) {

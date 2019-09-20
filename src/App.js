fc72b816ea604f93d0cead15450952de2750cab1
Yasser Carreón <yacasia@gmail.com>
2019-09-19 21:06:45 -0500

    [LS12] Use of Context

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const Button = (props) => {
-  const style =
-    props.theme === 'dark'
-      ? { background: '#666', color: '#fff' }
-      : { background: '#eee ', color: '#222' };
-  
-  return <button style={style}>{props.children}</button>;
-};
+// Doc: https://reactjs.org/docs/context.html
 
-const Toolbar = props => (
+/*
+  Context provides a way to pass data through the component
+  tree without having to pass props down manually at every level.
+*/
+const ThemeContext = React.createContext('light');
+
+const Button = (props) => (
+  <ThemeContext.Consumer>
+    {(theme) => {
+      const style =
+        theme === 'dark'
+          ? { background: '#666', color: '#fff' }
+          : { background: '#eee ', color: '#222' };
+      
+        return <button style={style}>{props.children}</button>;
+    }}
+  </ThemeContext.Consumer>
+);
+
+/*
+  A component in the middle doesn't have to
+  pass the theme down explicitly anymore.
+*/
+const Toolbar = () => (
   <div>
-    <Button theme={props.theme}>Edit</Button>
-    <Button theme={props.theme}>Save</Button>
+    <Button>Edit</Button>
+    <Button>Save</Button>
   </div>
 );
 
@@ -21,7 +37,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Toolbar theme="dark" />
+        {/*
+          Use a Provider to pass the current theme to the tree below.
+          Any component can read it, no matter how deep it is.
+          Toolbar doesn't have to pass the theme down explicitly anymore. 
+          
+          In this example, we're passing 'dark' as the current value.
+        */}
+        <ThemeContext.Provider value='dark'>
+          <Toolbar />
+        </ThemeContext.Provider>
       </div>
     );
   }

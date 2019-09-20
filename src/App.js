394a1387467eafc3d1ce1f8a096665f1872ba29b
Yasser Carreón <yacasia@gmail.com>
2019-09-19 20:46:28 -0500

    [LS12] Create playground

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+const Button = (props) => {
+  const style =
+    props.theme === 'dark'
+      ? { background: '#666', color: '#fff' }
+      : { background: '#eee ', color: '#222' };
+  
+  return <button style={style}>{props.children}</button>;
+};
+
+const Toolbar = props => (
+  <div>
+    <Button theme={props.theme}>Edit</Button>
+    <Button theme={props.theme}>Save</Button>
+  </div>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Toolbar theme="dark" />
+      </div>
+    );
+  }
 }
 
 export default App;
